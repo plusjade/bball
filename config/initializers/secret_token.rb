55b857f9ab1664319cfaad058cec10a9d2e687db
plusjade <plusjade@gmail.com>
2011-05-19 15:56:30 -0700

    fresh rails project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Bball::Application.config.secret_token = '5eb62f0b93fc5b1850e6e7ad2d6d7be765fd1577b882aa97f0fd6d8b7d858541d093d3ac8a61050f85ea216d395a5d658953bd3a0a24a8ac60ad174e1d82db06'

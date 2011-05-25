009d5ac542020cbb3bef99ccaef3f56c43801601
plusjade <plusjade@gmail.com>
2011-05-24 21:45:26 -0700

    refactor js to work with app object code

--- public/javascripts/apron/lib/app.js
+++ public/javascripts/apron/lib/app.js
@@ -1,13 +1,52 @@
 var app = {
+  $actionsW : null,
+  $playersW : null,
   
   offensiveActions : ["three", "two", "layup", "freethrow", "orebound", "assist", "turnover"],
   defensiveActions : ["steal", "block", "drebound", "foul", "charge"],
   pointValues : {"three": 3, "two": 2, "layup": 2, "freethrow": 1},
   
+  init : function(){
+    app.$actionsW = $("#actions_wrapper");
+    app.$playersW = $("#players_wrapper");
+
+  /* actions interface */  
+    app.$actionsW.find("div.action").live("click", function(){
+      app.$actionsW.find("div.action").removeClass("active");
+      app.$playersW.addClass("active");
+      $(this).addClass("active");
+    })
+          
+  /* record the data */  
+    app.$playersW.find("div.js_record").live("click", function(){
+      if(app.$actionsW.find("div.action.active").length === 0) return false;
+      app.recordData(app.keyize($(this)));
+      app.updateScore();
+      console.log(localStorage);
+
+      app.$playersW.removeClass("active");
+      app.$actionsW.find("div.action").removeClass("active");
+    })
+  },
+  
+  bindHover : function(){
+    app.$playersW.find("div.action").hover(function(){
+      var actionType = app.$actionsW.find("div.action.active").first().attr("rel");
+      
+      $(this).find("div.number").hide();
+      if(actionType === "shot"){
+        $(this).find("div.assign").hide();
+      }
+    },function(){
+      $(this).find("div.number").show();
+      $(this).find("div.assign").show();
+    });
+  },
+  
   keyize : function($node){
     var val = $node.attr("rel");
     var player = $node.parent()[0].id;
-    var action = $actionsW.find("div.action.active").first()[0].id;
+    var action = app.$actionsW.find("div.action.active").first()[0].id;
     return player + "." + action + "." + val;
   },
   
